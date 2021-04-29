e32c8656b7578658c1d827fdf602e8d6e3b3a647
Alex Christenson <christenson10@gmail.com>
2021-04-28 21:10:47 -0400

    before merge

--- weather.js
+++ weather.js
@@ -3,7 +3,9 @@ var dropL = document.querySelector("#drop-list")
 
 //Variable to fetch weather info
 function myFunction() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Flint&appid=api_key=662a8e54640cc40085727afa832a2eb6")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q={Flint}&appid=api_key=144038776569eb157fc421eeaef8f89e")
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    
     .then(function(response) {
         return response.json();
     })
@@ -13,12 +15,3 @@ function myFunction() {
     }
 
     myFunction();
-
-    // // make a get request to url
-    // fetch(apiUrl).then(function (response) {
-    //   console.log(response);
-    //   response.json().then(function (data) {
-    //     displayRepos(data, user));
-    //   });
-    // });
-
