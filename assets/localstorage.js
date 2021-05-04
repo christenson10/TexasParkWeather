6f0ece361bf89d10555fb14040ab8ba3c7ebda8c
Miller <rmillerwhite94@gmail.com>
2021-05-03 20:29:33 -0500

    add local storage js

---
+++ assets/localstorage.js
@@ -0,0 +1,12 @@
+var searchInput = document.querySelector('#searchSelection');
+var msgDiv = document.querySelector('#msg');
+var userSearchSpan = 
+
+function renderLastSearch() {
+    //Retrieve last search
+    var result = localStorage.getItem('#search-selection')
+}
+
+renderLastSearch();
+
+
